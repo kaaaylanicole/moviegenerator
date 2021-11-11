74d7a3c886b9cce0717274a9b8e6e2ffe792cf27
Kayla Coursey <kaaayla08@gmail.com>
2021-11-11 11:39:11 -0500

    first JS requirement

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-HELP
+fetch("movie_list.json")
+        .then(response => response.json())
+        .then(data => {
+            var movie_list = document.getElementById('movie_list')
+            for (var i = 0; i < data.length; i++) {
+                // var div = document.createElement("div");
+                // div.innerHTML = 'Movie: ' + data[i].movie.title;
+                console.log(data[i])
+                movie_list.innerHTML = movie_list.innerHTML + '<p>' + data[i].title + '</p>'
+            }
+        })
