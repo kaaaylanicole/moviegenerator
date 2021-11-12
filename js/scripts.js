d70bdc80838ad336a96b0c52a37574e63c925667
Kayla Coursey <kaaayla08@gmail.com>
2021-11-11 22:17:07 -0500

    finished one js requirement and updated the styling

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,18 @@ fetch("movie_list.json")
         .then(data => {
             var movie_list = document.getElementById('movie_list')
             for (var i = 0; i < data.length; i++) {
-                // var div = document.createElement("div");
-                // div.innerHTML = 'Movie: ' + data[i].movie.title;
                 console.log(data[i])
                 movie_list.innerHTML = movie_list.innerHTML + '<p>' + data[i].title + '</p>'
             }
         })
+
+function showForm() {
+    let formDiv = document.getElementById('signup-form')
+    console.log(formDiv)
+    formDiv.classList.remove('hide')
+    }
+
+function hideForm() {
+    let formDiv = document.getElementById('signup-form')
+    formDiv.classList.add('hide')
+    }
