2f622aa2d0f982c3718a2ea36d85d39449f84c7c
Kayla Coursey <kaaayla08@gmail.com>
2021-11-14 14:19:31 -0500

    second js requirement and some styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Read and Parse JSON file of movies and display on the page
 fetch("movie_list.json")
         .then(response => response.json())
         .then(data => {
@@ -5,9 +6,11 @@ fetch("movie_list.json")
             for (var i = 0; i < data.length; i++) {
                 console.log(data[i])
                 movie_list.innerHTML = movie_list.innerHTML + '<p>' + data[i].title + '</p>'
+                movie_list.innerHTML = movie_list.innerHTML + '<img src = "../images/.jpg"'
             }
         })
 
+// Popup box for the Create An Account Signup form
 function showForm() {
     let formDiv = document.getElementById('signup-form')
     console.log(formDiv)
@@ -18,3 +21,12 @@ function hideForm() {
     let formDiv = document.getElementById('signup-form')
     formDiv.classList.add('hide')
     }
+
+// Create a Form and Save the Values to TXT file
+function saveDynamicDataToFile() {
+
+    var userInput = document.getElementById("email").value;
+    
+    var blob = new Blob([userInput], { type: "text/plain;charset=utf-8" });
+    saveAs(blob, "dynamic.txt");
+}
