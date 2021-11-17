8388778f0b870d1cba9ce1aa892ce9913e385b4b
Kayla Coursey <kaaayla08@gmail.com>
2021-11-16 21:23:47 -0500

    third js requirement and additional content

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,64 @@
+var movieList = []
+
 // Read and Parse JSON file of movies and display on the page
 fetch("movie_list.json")
-        .then(response => response.json())
-        .then(data => {
-            var movie_list = document.getElementById('movie_list')
-            for (var i = 0; i < data.length; i++) {
-                console.log(data[i])
-                movie_list.innerHTML = movie_list.innerHTML + '<div class="movie-wrapper"> <p>' + data[i].title + '</p> <img src ="../images/' + data[i].img + '"></div>'            
-            }
-        })
+    .then(response => response.json())
+    .then(data => {
+        var movie_list = document.getElementById('movie_list')
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i])
+            movieList.push(data[i])
+            movie_list.innerHTML = movie_list.innerHTML + '<div class="movie-wrapper"> <p>' + data[i].title + '</p> <img src ="../images/' + data[i].img + '"></div>'
+        }
+    })
 
 // Popup box for the Create An Account Signup form
 function showForm() {
     let formDiv = document.getElementById('signup-form')
     console.log(formDiv)
     formDiv.classList.remove('hide')
-    }
+}
 
 function hideForm() {
     let formDiv = document.getElementById('signup-form')
     formDiv.classList.add('hide')
-    }
+}
 
 // Create a Form and Save the Values to TXT file
 function saveDynamicDataToFile() {
 
     var userInput = document.getElementById("email").value;
-    
+
     var blob = new Blob([userInput], { type: "text/plain;charset=utf-8" });
     saveAs(blob, "dynamic.txt");
 }
 
-// Create and use a function that accepts two or more values, calculates or determines a new value based on those inputs, and returns a new value
-// if (genre.includes('Action'))  {
-//     return data[i].movie
-// } else {
-//     return "No Movie Available"
-// }
+// Create an array, populate it with values, retreive values and display it
+
+function filterMovies() {
+    var amount = document.querySelector('#movie_amount').value
+
+    var moviesDisplay = movieList.filter(genreYearFilter)
+    console.log(moviesDisplay)
+
+    var movie_list = document.getElementById('movie_list')
+    movie_list.innerHTML = ''
+    for (var i = 0; i < moviesDisplay.length; i++) {
+        movie_list.innerHTML = movie_list.innerHTML + '<div class="movie-wrapper"> <p>' + moviesDisplay[i].title + '</p> <img src ="../images/' + moviesDisplay[i].img + '"></div>'
+        if (i == amount - 1) {
+            return
+        }
+    }
+}
+
+function genreYearFilter(movie) {
+    var genre = document.querySelector('#movie_genre').value
+    var year = document.querySelector('#movie_year').value
+
+    year = year.split('-')
+
+    var yearStart = parseInt(year[0])
+    var yearEnd = parseInt(year[1])
+
+    return movie.genre == genre && parseInt(movie.year) >= yearStart && parseInt(movie.year) <= yearEnd
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Reelz Movie Generator
+
+My project is called Reelz Movie Generator. I'm a huge movie fan and always have a hard time picking my next movie to watch. Users can come to the site to choose the amount of movies they are looking for, the genre they want to watch, and the year they would like the movie to be from. Once filters are chosen, the movies that match those filters will be displayed. I also have a "Sign Up" form for users to create an account. This could be cool in the future to have the accounts, a login portal and have users be able to save the movies they want to watch in their account.
+
+## Responsive Design
+- Desktop and Mobile
+- I have a media query for mobile: ```@media only screen and (max-width: 541px)```
+
+## Special Instructions
+- I wanted to try to implement ExpressJS in order to have a server that didn't make someone download an extension, but struggled a bit. I want to do this on my own later.
+- Instead, please open on VSCode and install Live Server [(https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)] in order to run this project.
+- When viewing the project, right click on the index.html file in VSCode and click "Open With Live Server" to view properly.
+
+## Requirements / Features List
+**1. Read and parse an external file (such as JSON or CSV) into your application and display some data from that in your app**
+- I  created a JSON file of movies (title, genre, year, and the movie's cover image) and displayed all of the movies on the page.
+**2. Create a form and save the values (on click of Submit button) to an external file**
+- I created a Sign Up form and am saving the values to a TXT file.
+**3. Create an array, dictionary or list, populate it with multiple values, retrieve at least one value, and use or display it in your application**
+- Within the movies I am displaying, I created an array from the JSON file, and retrieves the values that match filters to then display that onto the page.
+
+Thank you! :)
